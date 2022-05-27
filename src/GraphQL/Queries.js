8aa17fbd3c0e50e80235b639eca5574fa0bcbcae
schm00g <hk8igureu@relay.firefox.com>
2022-05-27 19:23:50 +0100

    queries

--- src/GraphQL/Queries.js
+++ src/GraphQL/Queries.js
@@ -2,11 +2,27 @@ import { gql } from '@apollo/client';
 
 export const GET_ALL_SPACE_CENTERS = gql`
     query GetFirstPageSpaceCenters {
-        spaceCenters(page: 1) {
+        spaceCenters(page: 1, pageSize: 9) {
             nodes {
                 id
                 name
             }
         }
     }
-`
+`;
+
+// TODO: rename
+export const GET_DESTINATIONS_FROM_PLANET = gql`
+    query GetDestinationsFromPlanet($from: Int!) {
+        flights(from: $from) {
+            nodes {
+                departureAt
+                landingSite {
+                    planet {
+                        name
+                    }
+                }
+            }
+        }
+    }
+`;
