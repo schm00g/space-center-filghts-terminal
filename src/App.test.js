e4da9ae1e668b159e13542d0f3648fb25f6ef91c
schm00g <hk8igureu@relay.firefox.com>
2022-06-02 23:42:20 +0100

    tidy up

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('render title', () => {
-  render(<App />);
-  const title = screen.getByText(/Spacious/i);
-  expect(title).toBeInTheDocument();
-});
